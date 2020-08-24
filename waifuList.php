8e37ab2f077ab0d4814a53525c1638635a904e30
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-08-24 21:53:25 +0700

    Update waifuList.php

--- waifuList.php
+++ waifuList.php
@@ -1,1 +1,1 @@
-[{"image":"https:\/\/i.ibb.co\/QKBQFrZ\/Hinata-47.jpg","name":"Hinata","title":"Boruto"}, {"image":"https:\/\/i.ibb.co\/QKBQFrZ\/Hinata-47.jpg","name":"Hinatod","title":"Boruto"}]
+[{"image":"https:\/\/i.ibb.co\/QKBQFrZ\/Hinata-47.jpg","name":"Hinatad","title":"Boruto"}, {"image":"https:\/\/i.ibb.co\/QKBQFrZ\/Hinata-47.jpg","name":"Hinatod","title":"Boruto"}]
