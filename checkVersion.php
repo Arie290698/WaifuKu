4999389a33b994ced958376d75b7266d579d5f0c
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-08-31 08:15:52 +0700

    Update checkVersion.php

--- checkVersion.php
+++ checkVersion.php
@@ -2,7 +2,7 @@
   "build": "15", 
   "ads": "1", 
   "admob": "0",
-  "startapp": "1,
+  "startapp": "1",
   "adcolony": "0",
   "interval": "10", 
   "banner": "ca-app-pub-8564552661823043/1557852071", 
