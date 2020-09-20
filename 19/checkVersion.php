db00884b5fee9025cb923cca23ad85041ecc9f96
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-09-20 15:08:43 +0700

    Update checkVersion.php

--- 19/checkVersion.php
+++ 19/checkVersion.php
@@ -1,7 +1,12 @@
 { 
-  "build": "19",
-  "interval": "25",
-  "banner": "ca-app-pub-8564552661823043/1557852071",
-  "reward": "ca-app-pub-8564552661823043/6389158346",
-  "site": "https:\/\/raw.githubusercontent.com\/Arie290698\/WaifuKu\/master\/19"
+  "build": "18", 
+  "ads": "0", 
+  "admob": "0",
+  "startapp": "1",
+  "interval": "25", 
+  "banner": "ca-app-pub-3940256099942544/6300978111", 
+  "reward": "ca-app-pub-3940256099942544/5224354917",
+  "inter": "ca-app-pub-3940256099942544/1033173712",
+  "site": "https:\/\/serarinne.my.id\/mypanel\/waifuku",
+  "review": "0"
 }
