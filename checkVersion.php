9c513e109ebc30d38cd8488c9393bc03f60eafda
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-09-23 05:21:27 +0700

    Update checkVersion.php

--- checkVersion.php
+++ checkVersion.php
@@ -5,7 +5,7 @@
   "startapp": "1",
   "interval": "8", 
   "banner": "ca-app-pub-8564552661823043/1557852071", 
-  "reward": "ca-app-pub-8564552661823043/6389158346",
+  "reward": "ca-app-pub-8564552661823043/2364327394",
   "inter": "ca-app-pub-8564552661823043/4607153836",
   "site": "https:\/\/serarinne.my.id\/mypanel\/waifuku",
   "review": "0"
