8a4ac8b4cf793889a5eaca1dc32ae39839d39fba
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-09-24 05:26:43 +0700

    Update checkVersion.php

--- checkVersion.php
+++ checkVersion.php
@@ -7,6 +7,6 @@
   "banner": "ca-app-pub-8564552661823043/1557852071", 
   "reward": "ca-app-pub-8564552661823043/6389158346",
   "inter": "ca-app-pub-8564552661823043/4607153836",
-  "site": "https:\/\/serarinne.my.id\/mypanel\/waifuku",
+  "site": "https:\/\/raw.githubusercontent.com\/Arie290698\/WaifuKu\/master",
   "review": "0"
 }
