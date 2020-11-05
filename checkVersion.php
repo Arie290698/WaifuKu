9a7476e8fad46677f275fe54627b55ec386740f9
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-11-05 15:41:56 +0700

    Update checkVersion.php

--- checkVersion.php
+++ checkVersion.php
@@ -6,8 +6,8 @@
   "interval": "8",
   "timeAds": "3",
   "packageName": "serarinne.waifuku",
-  "banner": "ca-app-pub-8564552661823043/1557852071", 
-  "reward": "ca-app-pub-8564552661823043/6389158346",
+  "banner": "ca-app-pub-8564552661823043/9233156917", 
+  "reward": "ca-app-pub-8564552661823043/2667748560",
   "inter": "ca-app-pub-8564552661823043/4607153836",
   "site": "https:\/\/raw.githubusercontent.com\/Arie290698\/WaifuKu\/master",
   "review": "0"
