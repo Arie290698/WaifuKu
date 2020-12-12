060bacaff277544abfaac6f972616f398a0d832e
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-12-12 16:25:49 +0700

    Update checkVersion.php

--- checkVersion.php
+++ checkVersion.php
@@ -6,6 +6,7 @@
   "interval": "15",
   "timeAds": "5",
   "packageName": "serarinne.waifuku",
+  "open": "ca-app-pub-8564552661823043/8479681121",
   "banner": "ca-app-pub-8564552661823043/9233156917", 
   "reward": "ca-app-pub-8564552661823043/2667748560",
   "inter": "ca-app-pub-8564552661823043/4607153836",
