132eeba445eb28a9224d06d1ef83a5d9b51b9201
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-12-15 14:58:33 +0700

    Update checkVersion.php

--- checkVersion.php
+++ checkVersion.php
@@ -1,7 +1,7 @@
 { 
   "build": "103", 
   "kedebug": "0", 
-  "interval": "10",
+  "interval": "5",
   "timeAds": "5",
   "packageName": "serarinne.waifuku",
   "open": "ca-app-pub-8564552661823043/8479681121",
